c1f527ca37496f0bf12a9362b226f215f9120824
fess <dreamer.alex.toms@gmail.com>
2021-10-07 23:02:29 +0300

    Initial commit

---
+++ app/src/main/java/com/dreamer/notesonjetpack/NoteApp.kt
@@ -0,0 +1,7 @@
+package com.dreamer.notesonjetpack
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApp : Application()
---
+++ app/src/main/java/com/dreamer/notesonjetpack/feature_note/domain/use_case/DeleteNote.kt
@@ -0,0 +1,13 @@
+package com.dreamer.notesonjetpack.feature_note.domain.use_case
+
+import com.dreamer.notesonjetpack.feature_note.domain.model.Note
+import com.dreamer.notesonjetpack.feature_note.domain.repository.NoteRepository
+
+class DeleteNote(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(note: Note) {
+        repository.deleteNote(note)
+    }
+}
